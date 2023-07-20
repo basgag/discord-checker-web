511a44968219e890a8957f3cadf0399b98666ca0
masterjanic <68118279+masterjanic@users.noreply.github.com>
2023-07-20 12:09:16 +0200

    feat: implement google adsense

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,6 +8,7 @@ import clsx from "clsx";
 import { Poppins } from "next/font/google";
 import { DefaultSeo } from "next-seo";
 import SEO from "next-seo.config";
+import Script from "next/script";
 
 export const poppins = Poppins({
   weight: ["300", "400", "500", "600", "700"],
@@ -21,12 +22,20 @@ const MyApp: AppType<{ session: Session | null }> = ({
   pageProps: { session, ...pageProps },
 }) => {
   return (
-    <SessionProvider session={session}>
-      <div className={clsx(poppins.variable, "min-h-screen font-sans")}>
-        <DefaultSeo {...SEO} />
-        <Component {...pageProps} />
-      </div>
-    </SessionProvider>
+    <>
+      <Script
+        async={true}
+        crossOrigin="anonymous"
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2388068147536440"
+        strategy="afterInteractive"
+      />
+      <SessionProvider session={session}>
+        <div className={clsx(poppins.variable, "min-h-screen font-sans")}>
+          <DefaultSeo {...SEO} />
+          <Component {...pageProps} />
+        </div>
+      </SessionProvider>
+    </>
   );
 };
 
