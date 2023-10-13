dc2481771f152f2d1400d781728e12934b2b0108
masterjanic <68118279+masterjanic@users.noreply.github.com>
2023-10-13 13:16:46 +0200

    feat: design flaw

--- src/components/admin/dashboard/AccountStats.tsx
+++ src/components/admin/dashboard/AccountStats.tsx
@@ -79,7 +79,7 @@ const AccountStats: React.FC = () => {
 
   if (!stats) {
     return (
-      <div className="grid grid-cols-1 grid-rows-1 gap-4 md:grid-cols-3">
+      <div className="col-span-full grid grid-cols-1 grid-rows-1 gap-4 overflow-hidden md:grid-cols-3">
         {entries.map((entry) => {
           return (
             <SkeletonStatsCard entry={entry} key={`skel-stats-${entry.key}`} />
